3bbfa4dd66df3be0207008a0557e2f27d99e2dfd
Kleinccx <124169356+Kleinccx@users.noreply.github.com>
2023-02-24 11:44:39 +0800

    Add files via upload

---
+++ MissU/MissU/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace MissU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Top20 = new System.Windows.Forms.ListBox();
+            this.Top10 = new System.Windows.Forms.ListBox();
+            this.Top3 = new System.Windows.Forms.ListBox();
+            this.Top1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Top20
+            // 
+            this.Top20.FormattingEnabled = true;
+            this.Top20.Location = new System.Drawing.Point(12, 106);
+            this.Top20.Name = "Top20";
+            this.Top20.Size = new System.Drawing.Size(120, 290);
+            this.Top20.TabIndex = 0;
+            this.Top20.SelectedIndexChanged += new System.EventHandler(this.Top20_SelectedIndexChanged);
+            // 
+            // Top10
+            // 
+            this.Top10.FormattingEnabled = true;
+            this.Top10.Location = new System.Drawing.Point(228, 106);
+            this.Top10.Name = "Top10";
+            this.Top10.Size = new System.Drawing.Size(134, 290);
+            this.Top10.TabIndex = 0;
+            // 
+            // Top3
+            // 
+            this.Top3.FormattingEnabled = true;
+            this.Top3.Location = new System.Drawing.Point(453, 106);
+            this.Top3.Name = "Top3";
+            this.Top3.Size = new System.Drawing.Size(129, 290);
+            this.Top3.TabIndex = 0;
+            // 
+            // Top1
+            // 
+            this.Top1.FormattingEnabled = true;
+            this.Top1.Location = new System.Drawing.Point(668, 106);
+            this.Top1.Name = "Top1";
+            this.Top1.Size = new System.Drawing.Size(120, 290);
+            this.Top1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "DisplayTop20";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(155, 215);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(155, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Undo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(382, 215);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Add";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(382, 244);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(52, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Undo";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(601, 215);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(44, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Add";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(601, 244);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(44, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Undo";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Top 20 Candidates";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Top 10 Candidates";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(472, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Top 3 Candidates";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(694, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ms Universe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Top1);
+            this.Controls.Add(this.Top3);
+            this.Controls.Add(this.Top10);
+            this.Controls.Add(this.Top20);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Top20;
+        private System.Windows.Forms.ListBox Top10;
+        private System.Windows.Forms.ListBox Top3;
+        private System.Windows.Forms.ListBox Top1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MissU/MissU/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool buttonGenerated = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines("MissUniverse.txt");
+
+            foreach (string line in lines)
+            {
+                // Add each line to the ListBox control.
+                Top20.Items.Add(line);
+            }
+           
+         if (!buttonGenerated)
+          {
+        Button newButton = new Button();
+        newButton.Text = "Click me!";
+         Top20.Items.Add(newButton);
+
+        buttonGenerated = true;
+        }
+        }
+        private void Top20_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var selectedItems = Top20.SelectedItems;
+
+            foreach (var item in selectedItems)
+            {
+
+
+                if (Top10.Items.Count <= 9)
+                {
+                    Top10.Items.Add(item);
+                }
+                else
+                {
+                    MessageBox.Show("You can only add up to 10 candidates to the list.");
+                }
+
+            }
+   
+            
+        }
+         private void button3_Click(object sender, EventArgs e)
+        {
+            var selectedItem = Top10.SelectedItem;
+
+            if (selectedItem != null)
+            {
+                Top10.Items.Remove(selectedItem);
+            }
+      
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var selectedItems = Top10.SelectedItems;
+
+            foreach (var item in selectedItems)
+            {
+
+
+                if (Top3.Items.Count<= 2)
+                {
+                    Top3.Items.Add(item);
+                }
+                else
+                {
+                    MessageBox.Show("You can only add up to 3 candidates to the list.");
+                }
+
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            var selectedItem = Top3.SelectedItem;
+
+            if (selectedItem != null)
+            {
+                Top3.Items.Remove(selectedItem);
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var selectedItems = Top3.SelectedItems;
+
+            foreach (var item in selectedItems)
+            {
+
+
+                if (Top1.Items.Count <= 0)
+                {
+                    Top1.Items.Add(item);
+                    MessageBox.Show(item+" is the new Miss Universe");
+                }
+                else
+                {
+                    MessageBox.Show("You can only add up to 1 candidate for Ms.Universe to the list.");
+                }
+            }
+
+
+            }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var selectedItem = Top1.SelectedItem;
+
+            if (selectedItem != null)
+            {
+                Top1.Items.Remove(selectedItem);
+            }
+
+        }
+    }
+    }
+
